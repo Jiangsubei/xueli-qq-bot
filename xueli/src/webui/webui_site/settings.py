a1af0dbad3acf702395b7ec5dd2253170f99efc5
Jiangsubei <fetdof905052@gmail.com>
2026-04-20 16:46:10 +0800

    忽略规则更新

--- xueli/src/webui/webui_site/settings.py
+++ xueli/src/webui/webui_site/settings.py
@@ -25,7 +25,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameViewMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "webui_site.urls"
